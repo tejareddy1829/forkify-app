8ee980bd0a6c71f363d84edb29d3815f9a4d318a
tejareddy1829 <tejar9700565670@gmail.com>
2022-11-10 19:48:54 +0530

    final

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '271575e9-e726-4c1c-a385-dbb5a59110a0';
 export const MODAL_CLOSE_SEC = 2.5;
--- src/js/view/searchView.js
+++
@@ -1,9 +0,0 @@
-class SearchView {
-  #parentEl = document.querySelector('.search');
-
-  getQuery() {
-    return this.#parentEl.querySelector('.search__field').value;
-  }
-}
-
-export default new SearchView();
